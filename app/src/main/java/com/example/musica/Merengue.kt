f49f70afcf6060afc7a489dac3bdbea35bf20f5e
jlianacastillo <lmov261220112@merida.tecnm.mx>
2023-03-22 17:20:49 -0600

    Ahora se puede reproducir canciones en la empty activity merengue

--- app/src/main/java/com/example/musica/Merengue.kt
+++ app/src/main/java/com/example/musica/Merengue.kt
@@ -1,11 +1,27 @@
 package com.example.musica
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Merengue : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_merengue)
+        var play=findViewById<Button>(R.id.play)
+        var stop=findViewById<Button>(R.id.stop)
+        var mp=MediaPlayer()
+        play.setOnClickListener {
+            mp.setDataSource(this, Uri.parse("android.resource://"+this.packageName+"/"+R.raw.mucis))
+            mp.prepare()
+            mp.start()
+        }
+        stop.setOnClickListener {
+            mp.stop()
+            mp.release()
+            mp=MediaPlayer()
+        }
     }
 }
